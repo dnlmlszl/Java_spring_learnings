79eee012d936efc93ecdac2fcb54dcde4075e279
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-07-19 20:59:15 +0200

    JPA complete

--- learning-jpa-and-hibernate/src/main/java/com/lmdlearning/learning_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ learning-jpa-and-hibernate/src/main/java/com/lmdlearning/learning_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.lmdlearning.learning_jpa_and_hibernate.course.JPA.CourseJPARepository;
+import com.lmdlearning.learning_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
@@ -12,20 +12,42 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 	// @Autowired
 	// private CourseJdbcRepository repository;
 	
+	// @Autowired
+	// private CourseJPARepository repository;
+	
 	@Autowired
-	private CourseJPARepository repository;
+	private CourseSpringDataJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		repository.insert(new Course(1, "Learn AWS JPA version!", "LMD"));
-		repository.insert(new Course(2, "Learn DevOps JPA version", "LMD"));
-		repository.insert(new Course(3, "Learn Mambo Banana JPA version", "LMD"));
+		//repository.insert(new Course(1, "Learn AWS JPA version!", "LMD"));
+		//repository.insert(new Course(2, "Learn DevOps JPA version", "LMD"));
+		//repository.insert(new Course(3, "Learn Mambo Banana JPA version", "LMD"));
 		
-		repository.deleteById(1);
+		// repository.deleteById(1);
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		// System.out.println(repository.findById(2));
+		// System.out.println(repository.findById(3));
+		
+		repository.save(new Course(1, "Learn AWS Spring JPA!", "LMD"));
+		repository.save(new Course(2, "Learn DevOps Spring JPA", "LMD"));
+		repository.save(new Course(3, "Learn Mambo Banana Spring JPA", "LMD"));		
+		repository.save(new Course(4, "Learn Spring JPA", "LMD"));
+		
+		repository.deleteById(1l);
+		
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+		System.out.println(repository.findById(4l));
+		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		
+		System.out.println(repository.findByAuthor("LMD"));
+		System.out.println(repository.findByAuthor(""));
+		
+		System.out.println(repository.findByName("Learn DevOps Spring JPA"));
 		
 	}	
 
---
+++ learning-jpa-and-hibernate/src/main/java/com/lmdlearning/learning_jpa_and_hibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,15 @@
+package com.lmdlearning.learning_jpa_and_hibernate.course.springdatajpa;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lmdlearning.learning_jpa_and_hibernate.course.Course;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+	
+	List<Course> findByAuthor(String author);
+	
+	List<Course> findByName(String name);
+
+}
